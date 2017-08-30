f966eda2261996cbd6ff349bed6b649c7e5633a4
EasonAndLily <15342349100@163.com>
2017-08-30 23:16:39 +0800

    Add image componect to section

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,9 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+//import ImageFigure from '../ImageFigure.js'
 
-
+//let ImageFigure = require('ImageFigure.js');
 let imageDatas = require('json-loader!../data/images.json');
 let yeomanImage = require('../images/001.jpg');
 imageDatas = (function generateImageURL(imageDatasArr) {
@@ -14,11 +15,52 @@ imageDatas = (function generateImageURL(imageDatasArr) {
     return imageDatasArr
 })(imageDatas);
 
-class AppComponent extends React.Component {
+
+class ImageFigure extends React.Component {
   render() {
+    return (
+      <figure className="img-figure">
+        <img src={this.props.data.singleImageData} alt={this.props.data.title}/>
+        <figcaption>
+            <h2 className="img-title">
+                {this.props.data.description}
+            </h2>
+        </figcaption>
+      </figure>
+    );
+  }
+}
+
+class AppComponent extends React.Component {
+	Constant: {
+		centerPos: { //中心坐标
+			left: 0,
+			right: 0
+		},
+		hPosRang: {//水平方向取值范围
+			leftSecX: [0, 0],
+			rightSecX: [0, 0],
+			Y: [0, 0]
+		},
+		vPosRang: {//垂直方向取值范围
+			x: [0, 0],
+			tpY : [0, 0]
+		}
+	},
+
+  render() {
+  	let controllerUnits = [];
+  	let imgFigures = [];
+  	imageDatas.forEach(function(val, index) {
+  		imgFigures.push(<ImageFigure data={val}/>);
+  	});
+
+
 	return (
 	  <section className="stage">
-		<section className="img-sec"></section>
+		<section className="img-sec">
+			{imgFigures}
+		</section>
 		<nav className="controller-nav"></nav>
 	  </section>
 	);
